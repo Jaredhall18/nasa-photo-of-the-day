ec3f135e15c388ed0b229aa2691ac0242939f435
jaredhall18 <jaredhall18@gmail.com>
2021-09-08 12:38:04 -0600

    Set up file structure and imported in APP.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
+import { useEffect, useState } from "react"
+import axios from 'axios'
+import {BASE_URL, API_KEY} from './constants'
+import Header from './components/Header'
+import Cards from './components/Cards'
+import Footer from './components/Footer'
 
 function App() {
   return (
---
+++ src/constants/index.js
@@ -0,0 +1,2 @@
+export const BASE_URL ='https://api.nasa.gov/planetary/apod'
+export const API_KEY = 'Cs1vOPkPliXAMe6hM276Bo1UfGQQauK0uXCltgYc'
